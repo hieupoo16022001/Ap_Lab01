5a6a64baf127310d1a525543b5748e944115421c
hieupoo16022001 <54589137+hieupoo16022001@users.noreply.github.com>
2020-05-19 14:30:31 +0700

    lab2

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace First_prg
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("this is my first program using C#");
+			Console.ReadLine();
+		}
+	}
+}
